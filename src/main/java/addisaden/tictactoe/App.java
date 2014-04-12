e2a7d36dbd78085f80880c11d3b48dc3ca28b5d8
Adrian E <addis.aden@gmail.com>
2014-04-12 10:34:32 +0200

    kann nun auf spielfeld setzen

--- src/main/java/addisaden/tictactoe/App.java
+++ src/main/java/addisaden/tictactoe/App.java
@@ -6,9 +6,13 @@ package addisaden.tictactoe;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
         Game the_game = new Game();
+
+        the_game.move(0);
+        the_game.move(1);
+        the_game.move(2);
 
         the_game.print_spielfeld();
     }
--- src/main/java/addisaden/tictactoe/Game.java
+++ src/main/java/addisaden/tictactoe/Game.java
@@ -4,12 +4,23 @@ class Game
 {
   private int[] spielfeld = new int[9];
 
+  private int current_move = 0;
+
   Game()
   {
     for(int i = 0; i < spielfeld.length; i++)
     {
       spielfeld[i] = 0;
     }
+  }
+
+  public void move(int position) throws Exception
+  {
+    is_valid_position(position);
+    is_empty_position(position);
+
+    spielfeld[position] = (current_move % 2) + 1;
+    current_move++;
   }
 
   public void print_spielfeld()
@@ -22,5 +33,24 @@ class Game
       if(i % 3 == 2)
         System.out.println();
     }
+  }
+
+  private void is_valid_position(int position) throws Exception
+  {
+    require(
+      position >= 0 || position < spielfeld.length,
+      "Position nicht vorhanden"
+    );
+  }
+
+  private void is_empty_position(int position) throws Exception
+  {
+    require(spielfeld[position] == 0, "Position ist nicht leer");
+  }
+
+  private void require (boolean condition, String message) throws Exception
+  {
+    if(!condition)
+      throw new Exception(message);
   }
 }
